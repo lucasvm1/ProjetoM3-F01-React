cae0110e52ca5a2f1ddc6e0801ab23dd41a43e1f
Lucas Viana <lucas.vianam@outlook.com>
2022-04-27 20:04:42 -0300

    home css e jsx

--- src/components/PaletaLista.jsx
+++ src/components/PaletaLista.jsx
@@ -1,22 +1,35 @@
-import "./PaletaLista.css"
+import "./PaletaLista.css";
 import { paletas } from "mocks/paletas.js";
 
 function PaletaLista() {
-    return <div className="PaletaLista">
+  return (
+    <div className="PaletaLista">
       {paletas.map((paleta, index) => (
-        <div className="PaletaListaItem">
+        <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
           <div>
             <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
-            <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)} </div>
-            <div className="PaletaListaItem__descricao"> {paleta.descricao} </div>
+            <div className="PaletaListaItem__preco">
+              R$ {paleta.preco.toFixed(2)}{" "}
+            </div>
+            <div className="PaletaListaItem__descricao">
+              {" "}
+              {paleta.descricao}{" "}
+            </div>
             <div className="PaletaListaItem__acoes Acoes">
-              <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+                adicionar
+              </button>
             </div>
           </div>
-            <img className="PaletaListaItem__foto" src={paleta.foto} alt={paleta.titulo} />
+          <img
+            className="PaletaListaItem__foto"
+            src={paleta.foto}
+            alt={paleta.titulo}
+          />
         </div>
       ))}
-      </div>
-      }
-  
-  export default PaletaLista;
+    </div>
+  );
+}
+
+export default PaletaLista;
