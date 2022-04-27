a510c50828043a528bb5e3acba56831514971dfa
Lucas Viana <lucas.vianam@outlook.com>
2022-04-27 18:51:51 -0300

    update paletas

--- src/components/PaletaLista.jsx
+++ src/components/PaletaLista.jsx
@@ -1,19 +1,22 @@
 import "./PaletaLista.css"
+import { paletas } from "mocks/paletas.js";
 
 function PaletaLista() {
     return <div className="PaletaLista">
-    <div className="PaletaListaItem">
-      <div>
-        <div className="PaletaListaItem__titulo"> Doce de Leite com Doce de Leite </div>
-        <div className="PaletaListaItem__preco">R$ 10,00</div>
-        <div className="PaletaListaItem__descricao"> Quam vulputate dignissim suspendisse in est ante in nibh mauris.  </div>
-        <div className="PaletaListaItem__acoes Acoes">
-          <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+      {paletas.map((paleta, index) => (
+        <div className="PaletaListaItem">
+          <div>
+            <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
+            <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)} </div>
+            <div className="PaletaListaItem__descricao"> {paleta.descricao} </div>
+            <div className="PaletaListaItem__acoes Acoes">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+            </div>
+          </div>
+            <img className="PaletaListaItem__foto" src={paleta.foto} alt={paleta.titulo} />
         </div>
+      ))}
       </div>
-        <img className="PaletaListaItem__foto" src={require("../assets/img/doce-de-leite-com-doce-de-leite.png")} alt="Paleta de Doce de Leite" />
-    </div>
-  </div>
-  }
+      }
   
   export default PaletaLista;
